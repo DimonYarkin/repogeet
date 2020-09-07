44c516a37424952682557580c96a7c4093e9cb7c
DimonYarkin <dmitryyarkin@yandex.ru>
2020-09-07 23:40:04 +0300

    исапрвлени сообщение пользователю

--- task6.py
+++ task6.py
@@ -4,6 +4,6 @@ result_days = 1
 result_km = a
 while result_km < b:
     result_km += a * 0.1
-    print(f"{result_days} й день {result_km:.2f} км")
+    print(f"{result_days} - й день {result_km:.2f} км")
     result_days += 1
 print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
