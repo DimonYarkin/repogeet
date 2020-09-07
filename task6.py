71a6822e7029568bab7aabf446da0db866f2ceb5
DimonYarkin <51164050+DimonYarkin@users.noreply.github.com>
2020-09-07 18:12:16 +0300

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,9 @@
+a = int(input("Введите результаты пробежки первого дня в км "))
+b = int(input("Введите общий желаемый результат в км "))
+result_days = 1
+result_km = a
+while result_km < b:
+    result_km += a * 0.1
+    print(f"{result_days} й день {result_km:.2f} км")
+    result_days += 1
+print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
