3a9433394c5d90ffc5ed0e0b271d0aa7d1344ed1
DimonYarkin <dmitryyarkin@yandex.ru>
2020-09-09 17:51:20 +0300

    изменено

--- task6.py
+++ task6.py
@@ -3,7 +3,7 @@ b = int(input("Введите общий желаемый результат в 
 result_days = 1
 result_km = a
 while result_km < b:
-    result_km += a * 0.1
+    result_km += result_km * 0.1
     print(f"{result_days} - й день {result_km:.2f} км")
     result_days += 1
 print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
